1fc6b61c683782ff9b8914ce6c624a4b41378231
GustavsK <gustavs.krasnikovs@edi.lv>
2024-06-03 10:18:03 +0300

    Feat: setup fix

--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", package])
 
 if __name__ == "__main__":
-    install('requirment.txt')
+    install('requirements.txt')
